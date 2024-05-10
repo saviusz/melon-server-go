871394fad4957c9f545798da65e5fae1c9edbeeb
Seweryn Woliński <seweryn.wolinski@swol.pl>
2024-05-11 00:56:42 +0200

    feat: Define API Errors
    Signed-off-by: Seweryn Woliński <seweryn.wolinski@swol.pl>

--- src/errors/errors.go
+++ src/errors/errors.go
@@ -7,13 +7,54 @@ import (
 
 type ApiError struct {
 	XMLName  xml.Name `json:"-" xml:"Error"`
-	Type     string   `json:"type" xml:",attr"`
+	Type     string   `json:"type" xml:",attr"` // Uri to documentation
 	Title    string   `json:"title"`
-	Instance string   `json:"instance"`
+	Instance string   `json:"instance"` // Identifier (or uri with details)
 	Detail   string   `json:"detail,omitempty"`
 	Code     int      `json:"status" xml:"-"`
 }
 
 func (err ApiError) Error() string {
 	return fmt.Sprintf("%v(%v) %v: %v", err.Code, err.Instance, err.Title, err.Detail)
+}
+
+func UnsupportedMediaError(mediatype string) *ApiError {
+
+	detail := fmt.Sprintf("Provided mediatype %q is not supported", mediatype)
+
+	return &ApiError{
+		Code:     415,
+		Title:    "Unsupported Media Type",
+		Detail:   detail,
+		Type:     "about:blank",
+		Instance: "elo", // TODO: Change me to random
+	}
+}
+
+func InternalServerError(err error) *ApiError {
+
+	// TODO: Insert logging
+
+	return &ApiError{
+		Code:     500,
+		Title:    "Internal Server Error",
+		Detail:   err.Error(),
+		Type:     "about:blank",
+		Instance: "elo", //TODO: Change me to random
+	}
+}
+
+func BadRequestError(desc string) *ApiError {
+	detail := &desc
+	if desc == "" {
+		detail = nil
+	}
+
+	return &ApiError{
+		Code:     400,
+		Title:    "Bad Request",
+		Detail:   *detail,
+		Type:     "about:blank",
+		Instance: "elo", //TODO: Change me to random
+	}
 }
